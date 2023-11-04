d2eab2529baa0d5a4cd5ad8da42878e316e8d65a
Belkin1337 <fank.tomphson@gmail.com>
2023-11-04 21:11:18 +0300

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,29 @@ import { I18nProvider } from '@/lib/next-international'
 import { AppProps } from 'next/app'
 
 import localFont from "next/font/local";
+import Head from 'next/head';
 const font = localFont({ src: "../public/font/Monocraft.otf" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <I18nProvider locale={pageProps.locale}>
-      <main className={font.className}>
-        <Component {...pageProps} />
-      </main>
-    </I18nProvider>
+    <>
+      <Head>
+        <title>pureawake | dev</title>
+        <meta name="description" content="Ку! Я pureawake. Здесь вы можете узнать больше обо мне, моих интересах и скиллах." />
+        <meta name="keywords" content="pureawake, личная страница, интересы, профиль" />
+        <meta name="author" content="pureawake" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="canonical" href="http://pureawake.ru/" />
+        <meta property="og:title" content="pureawake | dev" />
+        <meta property="og:description" content="Ку! Я pureawake. Здесь вы можете узнать больше обо мне, моих интересах и скиллах." />
+        <meta property="og:image" content="/images/avatar5.jpg" />
+        <meta property="og:url" content="https://pureawake.ru/" />
+      </Head>
+      <I18nProvider locale={pageProps.locale}>
+        <main className={font.className}>
+          <Component {...pageProps} />
+        </main>
+      </I18nProvider>
+    </>
   )
 }
